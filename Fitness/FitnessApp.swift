99f13b90eaf1b45e004280d3fbcff95c10ab9f09
twixour <twixour@gmail.com>
2023-06-30 21:29:48 +0530

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Raushan Kashyap on 30/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
